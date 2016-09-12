54cd54c72c49ca3ff647d1e4cb7e73153c6cc89a
limiaomaster <814534518@qq.com>
2016-09-12 14:01:11 +0800

    点击哪一行，就显示小喇叭，其他的不显示！

--- app/src/main/java/cn/edu/cdut/lm/speaker/SingleSongRVAdapter.java
+++ app/src/main/java/cn/edu/cdut/lm/speaker/SingleSongRVAdapter.java
@@ -9,56 +9,79 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 /**
  * Created by LimiaoMaster on 2016/8/24 18:37
  */
-class SingleSongRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+class SingleSongRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private  Context context;
-    private  List<Mp3Info> mp3InfoList;
+    private Context context;
+    private List<Mp3Info> mp3InfoList = new ArrayList<>();
     private int listPosition;
     private int lastPosition = -1;
 
-    SingleSongRVAdapter(Context context, List<Mp3Info> list) {
+    public SingleSongRVAdapter(Context context, List<Mp3Info> list) {
         this.context = context;
-        this.mp3InfoList = list;
+        this.mp3InfoList.addAll(list);
     }
+
+    public void addData(List<Mp3Info> list) {
+        if (list == null)
+            return;
+        mp3InfoList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    private void selectSongs(int position){
+        if(position>=mp3InfoList.size())
+            return;
+        for(int i=0;i<mp3InfoList.size();i++){
+            if(i==position){
+                mp3InfoList.get(i).setSelected(true);
+            }else {
+                mp3InfoList.get(i).setSelected(false);
+            }
+        }
+        notifyDataSetChanged();
+        Log.e("selectSongs","选中了您点击的歌曲！  "+position);
+    }
+
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View viewOfGeneralLines = LayoutInflater.from(context).inflate(R.layout.item_localmusic_singlesong,parent,false);
+        View viewOfGeneralLines = LayoutInflater.from(context).inflate(R.layout.item_localmusic_singlesong, parent, false);
         return new GeneralLinesViewHolder(viewOfGeneralLines);
     }
 
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-        Mp3Info mp3Info;
-            mp3Info = mp3InfoList.get(position);
-            ((GeneralLinesViewHolder) holder).title.setText(mp3Info.getTitle());
-            ((GeneralLinesViewHolder) holder).artist.setText(mp3Info.getArtist());
-            ((GeneralLinesViewHolder) holder).album.setText(mp3Info.getAlbum());
+        final Mp3Info mp3Info;
+        mp3Info = mp3InfoList.get(position);
+        ((GeneralLinesViewHolder) holder).title.setText(mp3Info.getTitle());
+        ((GeneralLinesViewHolder) holder).artist.setText(mp3Info.getArtist());
+        ((GeneralLinesViewHolder) holder).album.setText(mp3Info.getAlbum());
+        ((GeneralLinesViewHolder) holder).speaker.setVisibility(mp3Info.isSelected()?View.VISIBLE:View.GONE);
+        Log.e("onBindViewHolder","speaker，的显示已更新！ "+position+(mp3Info.isSelected()?View.VISIBLE:View.GONE));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp3Info.isSelected()){
+                    Log.e("onBindViewHolder","该行已经被选中，直接返回！");
+                    return;
+                }
+                selectSongs(position);
+                Log.e("onBindViewHolder","该行尚未被选中，已经把它设为选中状态！ "+position);
 
-            /*((GeneralLinesViewHolder) holder).view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    listPosition = position;
-                    if (listPosition != lastPosition) {
-                            setThisItemSelected(position);
-                            if (mp3InfoList.get(position).isSelected()){
-                                ((GeneralLinesViewHolder) holder).speaker.setVisibility(View.VISIBLE);
-                            }
-                        }
-                    lastPosition = listPosition;
-                    }
-            });*/
+            }
+        });
     }
 
     private void setThisItemSelected(int position) {
-        for (int i = 0 ; i<mp3InfoList.size(); i++){
+        for (int i = 0; i < mp3InfoList.size(); i++) {
             mp3InfoList.get(i).setSelected(position == i);
         }
         notifyDataSetChanged();
@@ -66,7 +89,7 @@ class SingleSongRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     @Override
     public int getItemCount() {
-       return mp3InfoList.size();
+        return mp3InfoList.size();
     }
 
     private class GeneralLinesViewHolder extends RecyclerView.ViewHolder {
@@ -76,6 +99,7 @@ class SingleSongRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         ImageView more;
         ImageView speaker;
         View view;
+
         GeneralLinesViewHolder(View viewOfGeneralLines) {
             super(viewOfGeneralLines);
             view = viewOfGeneralLines;
@@ -84,21 +108,21 @@ class SingleSongRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
             album = (TextView) viewOfGeneralLines.findViewById(R.id.album_localmusic);
             more = (ImageView) viewOfGeneralLines.findViewById(R.id.iv_more_localmusic);
             speaker = (ImageView) viewOfGeneralLines.findViewById(R.id.speaker);
-            viewOfGeneralLines.setOnClickListener(new View.OnClickListener() {
+            /*viewOfGeneralLines.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     listPosition = getAdapterPosition();
-                    Log.v("onClick()","您点击的位置是："+listPosition);
+                    Log.v("onClick()", "您点击的位置是：" + listPosition);
 
                     if (listPosition != lastPosition) {
                         setThisItemSelected(listPosition);
-                        if (mp3InfoList.get(listPosition).isSelected()){
+                        if (mp3InfoList.get(listPosition).isSelected()) {
                             speaker.setVisibility(View.VISIBLE);
                         }
                     }
                     lastPosition = listPosition;
                 }
-            });
+            });*/
         }
     }
 }
